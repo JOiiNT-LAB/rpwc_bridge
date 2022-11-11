41fec421f0e7083e1acd30e443bf176b88ff4233
marc-galusi <marc.galusi@electrasrl.com>
2022-11-11 16:46:48 +0100

    created callback for retrieving gripper current position

--- src/main_onrobot_twofg_bridge.cpp
+++ src/main_onrobot_twofg_bridge.cpp
@@ -3,26 +3,26 @@
 #include <rpwc_bridge/rpwc_gripper_cmd.h>
 #include <std_msgs/Float64.h>
 #include <onrobot_msgs/onrobot_gripper.h>
+#include <rpwc_bridge/rpwc_gripper_pos.h>
 
 
 
 ros::Publisher pub_gripper_des_;
 bool grasping_ = false;
 onrobot_msgs::onrobot_gripper cmd_msg_;
+std_msgs::Float64 curr_grip_;
 
 bool callback_rpwc_gripper_single_cmd(rpwc_bridge::rpwc_gripper_cmd::Request  &req, rpwc_bridge::rpwc_gripper_cmd::Response &res)
 {
 	if(req.EE_cmd_.data >= 0.5 && !grasping_)
 	{
 		grasping_ = true;
-		// cmd_msg_.position = int(45-(req.EE_cmd_.data*45));
 		cmd_msg_.position = int(0);
 		pub_gripper_des_.publish(cmd_msg_);
 	}
 	else if(req.EE_cmd_.data < 0.5 && grasping_)
 	{
 		grasping_ = false;
-		// cmd_msg_.position = int(45-(req.EE_cmd_.data*45));
 		cmd_msg_.position = int(45);
 		pub_gripper_des_.publish(cmd_msg_);
 	}
@@ -30,20 +30,15 @@ bool callback_rpwc_gripper_single_cmd(rpwc_bridge::rpwc_gripper_cmd::Request  &r
 	return true;
 }
 
+bool callback_EE_curr_pos(rpwc_bridge::rpwc_gripper_pos::Request &req, rpwc_bridge::rpwc_gripper_pos::Response &res)
+{
+	res.EE_pos_ = curr_grip_;
+	return true;
+}
+
 void callback_rpwc_gripper_cmd(const std_msgs::Float64::ConstPtr& msg)
 {
- //  	if(msg->data >= 0.5 && !grasping_)
-	// {
-	// 	grasping_ = true;
-	// 	cmd_msg_.position = int(0);
-	// 	pub_gripper_des_.publish(cmd_msg_);
-	// }
-	// else if(msg->data < 0.5 && grasping_)
-	// {
-	// 	grasping_ = false;
-	// 	cmd_msg_.position = int(45);
-	// 	pub_gripper_des_.publish(cmd_msg_);
-	// }
+	curr_grip_ = *msg;
 	cmd_msg_.position = int((msg->data*45)/0.038);
 	pub_gripper_des_.publish(cmd_msg_);
 }
@@ -72,7 +67,7 @@ int main(int argc, char **argv)
 
 	//Service Server
   	ros::ServiceServer server_rpwc_gripper_cmd = nh.advertiseService("rpwc_EE_single_cmd", &callback_rpwc_gripper_single_cmd);
-
+	ros::ServiceServer server_curr_grip_ = nh.advertiseService("rpwc_EE_curr_pos", &callback_EE_curr_pos);
 
 
 	while(ros::ok())
